e86f8b88a3d007e4f3fb910054e16a50bd5cac82
Paul Heller <95037561+paulffm@users.noreply.github.com>
2022-05-17 09:29:59 +0200

    Add files via upload

---
+++ Task4/2_dt_classification.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+# references:
+# https://scikit-learn.org/stable/auto_examples/tree/plot_tree_regression.html#sphx-glr-auto-examples-tree-plot-tree-regression-py
+# https://scikit-learn.org/stable/modules/tree.html
+
+import numpy as np
+from sklearn import tree
+from sklearn.datasets import load_iris
+from sklearn.model_selection import cross_validate
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+import utils
+
+
+def get_unsure_test_items(clf, X_test: np.ndarray) -> np.ndarray:
+    """Returns a boolean array where all items with a probability != 1 are marked as true"""
+    y_test = clf.predict_proba(X_test)
+    return y_test.max(axis=1) != 1
+
+
+def plot_decision_boundary(clf, X_train: np.ndarray, X_test: np.ndarray,
+                           y_train: np.ndarray, y_test: np.ndarray, y_test_unsure: np.ndarray) -> None:
+    """Create a decision boundary plot that shows the predicted label for each point."""
+    cache = {}
+    plt.figure()
+    h = 0.05  # step size in the mesh
+
+    # Create color maps
+    cmap_light = ListedColormap(["#FFAAAA", "#AAFFAA", "#AAAAFF"])
+    cmap_bold = ListedColormap(["#FF0000", "#00FF00", "#0000FF"])
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    # creating limits in graph -> class. model dont touch axes
+    x_min, x_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
+    y_min, y_max = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
+    # rectangular mesh with length x_max - x_min and breadth y_max - y_max with step size h
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    k = clf.min_samples_leaf
+    if k in cache:
+        Z = cache[k]
+    else:
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        cache[k] = Z
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    # Plot also the training and test points
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cmap_bold, marker="x", s=25, label="Training Points")
+    plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cmap_bold, marker="^", edgecolor="k", s=25, label="Test Points")
+    plt.scatter(X_test[y_test_unsure][:, 0], X_test[y_test_unsure][:, 1], c="yellow", marker="*", edgecolor="k", s=50, label="Unsure Points")
+    plt.legend()
+    # limitation of plot
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.show()
+
+
+def get_acc_per_max_depth(X: np.ndarray, y: np.ndarray, max_depths: range) -> [np.ndarray, np.ndarray]:
+    """Runs 10-fold cross validation for all given depths and
+     returns an array for the corresponding train and test accuracy"""
+
+    def evaluate(X, y, max_depth):
+        model = tree.DecisionTreeClassifier(max_depth=max_depth)
+
+        scores = cross_validate(estimator=model, X=X, y=y, scoring="accuracy", cv=10, return_train_score=True)
+        return np.mean(scores["train_score"]), np.mean(scores["test_score"])
+
+    # Evaluate each depth parameter
+    accuracies = np.array([evaluate(X, y, md) for md in max_depths])
+    acc_train = accuracies[:, 0]
+    acc_test = accuracies[:, 1]
+    return acc_train, acc_test
+
+
+def main():
+    # Load the iris dataset
+    iris = load_iris()
+
+    # Fit a Decision tree on the iris data
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(iris.data, iris.target)
+
+    utils.plot_decision_tree(clf, iris)
+
+    X, y = utils.reduce_iris_to_2d(iris)
+
+    # Train decision tree
+    clf = tree.DecisionTreeClassifier()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    clf = clf.fit(X_train, y_train)
+
+    y_test_unsure = get_unsure_test_items(clf, X_test)
+
+    plot_decision_boundary(clf, X_train, X_test, y_train, y_test, y_test_unsure)
+
+    # Make experiments reproducible
+    np.random.seed(0)
+
+    # Define interval
+    max_depths = range(1, 16)
+    acc_train, acc_test = get_acc_per_max_depth(X, y, max_depths)
+
+    utils.plot_results(acc_test, acc_train, max_depths)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Task4/3_dt_regression.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+import utils
+
+
+def get_dt_prediction(X_train: np.ndarray, y_train: np.ndarray, max_depth: int) -> np.ndarray:
+    """Fits a decision tree regressor and returns its predictions as an array."""
+    model = DecisionTreeRegressor(max_depth=max_depth)
+    model.fit(X_train, y_train)
+    return model.predict(X_train)
+
+
+def plot_regression_models(X_cont: np.ndarray, y_cont: np.ndarray, y_1: np.ndarray, y_2: np.ndarray) -> None:
+    """Plots the regression results for y_1 and y_2 on top of the training data X_cont, y_cont."""
+    plt.figure(figsize=(12, 4))
+    # subplot(nrow, ncol, index)
+    ax = plt.subplot(121)
+    plt.scatter(X_cont, y_cont, marker='o', edgecolors='black')
+    plt.plot(X_cont, y_1, label='max depth', color='orange')
+    ax.legend()
+    ax.set_ylabel("Target")
+    ax.set_xlabel("Data")
+    ax = plt.subplot(122)
+    plt.scatter(X_cont, y_cont, s=20, edgecolor="black")
+    plt.plot(X_cont, y_2, label="$max\_depth=5$", color="green")
+    ax.legend()
+    plt.xlabel("Data")
+    plt.suptitle("Decision Tree Regression")
+    plt.show()
+
+
+
+
+def main():
+    np.random.seed(42)
+    # create dataset
+    X_train, y_train = utils.distorted_sin()
+    utils.plot_points(X_train, y_train)
+
+    y_1, y_2 = [get_dt_prediction(X_train, y_train, max_depth) for max_depth in [2, 5]]
+
+    # Plot the results
+    plot_regression_models(X_train, y_train, y_1, y_2)
+
+
+if __name__ == '__main__':
+    main()
